57411204f05f3274e05c0ffeeb4628defd6d1721
Mesh <meshmwangi828@gmail.com>
2025-10-18 16:05:57 +0300

    dash

--- mwasa/urls.py
+++ mwasa/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# 💡 Add these lines to customize the admin panel text
+admin.site.site_header = "MWASAMWADA WELL-BEING SERVICES ADMIN DASHBOARD"
+admin.site.site_title = "MWASAMWADA ADMIN PORTAL"
+admin.site.index_title = "Welcome to the MWASAMWADA Admin Dashboard"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('content.urls')),
